39ef1447b9d56fb9709b6bca8c00cbe38fbaa2fa
jinhuajin <1720068650@qq.com>
2019-10-23 11:23:03 +0800

    Updates

---
+++ precache-manifest.aa7eda2ffc81cc9febde68788eaa4b2d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d8553d9bc563647b7f33c21bf25d4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2db70d84a2afd2052550",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ac1dc7102dd11cfcb3a",
+    "url": "/static/js/2.f8fed60c.chunk.js"
+  },
+  {
+    "revision": "2db70d84a2afd2052550",
+    "url": "/static/js/main.e1757f65.chunk.js"
+  },
+  {
+    "revision": "d3aed32ad24d6c1a3562",
+    "url": "/static/js/runtime-main.28d95b28.js"
+  }
+]);
